4ddaa848e768f482ea16d3d969f5a5a6956442d8
Jimmy Gammell <jgammell@purdue.edu>
2025-02-20 14:18:32 -0500

    ...

--- src/train.py
+++ src/train.py
@@ -309,13 +309,14 @@ if init_from == 'resume':
 checkpoint = None # free up memory
 
 # compile the model
-if compile:
+if False: # compile:
     print("compiling the model... (takes a ~minute)")
     unoptimized_model = model
     model = torch.compile(model) # requires PyTorch 2.0
 
 # wrap model into DDP container
 if ddp:
+    print('putting model in DDP wrapper')
     model = DDP(model, device_ids=[ddp_local_rank])
 
 # helps estimate an arbitrarily accurate loss over either split using many batches
