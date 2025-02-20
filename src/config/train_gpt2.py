cfa9d39319b9b6be81aaff8933a59128452b1d94
Jimmy Gammell <jgammell@purdue.edu>
2025-02-20 15:28:05 -0500

    ...

--- src/config/train_gpt2.py
+++ src/config/train_gpt2.py
@@ -10,7 +10,7 @@ wandb_run_name='gpt2-124M'
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 gpu_count = 8
 total_batch_size = 960
-batch_size = 60
+batch_size = 120
 gradient_accumulation_steps = total_batch_size // batch_size
 assert gradient_accumulation_steps % gpu_count == 0
 
