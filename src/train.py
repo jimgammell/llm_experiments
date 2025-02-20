2371e54fb2fbda9407e8dd2ab7ea6a41da53c8ef
Jimmy Gammell <jgammell@purdue.edu>
2025-02-20 16:56:57 -0500

    ...

--- src/train.py
+++ src/train.py
@@ -259,7 +259,7 @@ checkpoint = None # free up memory
 if compile:
     print("compiling the model... (takes a ~minute)")
     unoptimized_model = model
-    model = torch.compile(model, mode='max-autotune') # requires PyTorch 2.0
+    model = torch.compile(model) # requires PyTorch 2.0
 
 # wrap model into DDP container
 if ddp:
