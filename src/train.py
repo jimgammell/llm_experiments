f0cfa4819c3c94f5b4adc8187c6dfaad3b1ef957
Jimmy Gammell <jgammell@purdue.edu>
2025-02-20 11:51:25 -0500

    Changed dataloading process to hopefully avoid delays due to loading things to/from disk, and take advantage of multiple CPU cores.

--- src/train.py
+++ src/train.py
@@ -24,12 +24,14 @@ from contextlib import nullcontext
 
 import numpy as np
 import torch
+from torch.utils.data import DataLoader, Dataset
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 
-os.environ['OMP_NUM_THREADS'] = '14'
+cpu_cores = 14
+os.environ['OMP_NUM_THREADS'] = f'{cpu_cores}'
 
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
@@ -119,22 +121,61 @@ ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torc
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # poor man's data loader
+#data_dir = os.path.join('data', dataset)
+#def get_batch(split):
+#    # We recreate np.memmap every batch to avoid a memory leak, as per
+#    # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+#    if split == 'train':
+#        data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+#    else:
+#        data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+#    ix = torch.randint(len(data) - block_size, (batch_size,))
+#    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+#    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+#    if device_type == 'cuda':
+#        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
+#        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+#    else:
+#        x, y = x.to(device), y.to(device)
+#    return x, y
+
+# Modifying the data loading process so that we can 1) store the whole dataset in RAM, and 2) take advantage of multiple CPU cores.
+class MyDataset(Dataset):
+    def __init__(self, data_dir, split):
+        super().__init__()
+        if split == 'train':
+            dataset = np.fromfile(os.path.join(data_dir, 'train.bin'), dtype=np.uint16)
+        elif split == 'val':
+            dataset = np.fromfile(os.path.join(data_dir, 'val.bin'), dtype=np.uint16)
+        else:
+            assert False
+    
+    def __getitem__(self, idx):
+        x = torch.from_numpy((self.dataset[idx:idx+block_size]).astype(np.int64))
+        y = torch.from_numpy((self.dataset[idx+1:idx+1+block_size]).astype(np.int64))
+        return x, y
+    
+    def __len__(self):
+        return len(self.dataset) - block_size
+
 data_dir = os.path.join('data', dataset)
+train_dataset = MyDataset(data_dir, 'train')
+val_dataset = MyDataset(data_dir, 'val')
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=cpu_cores)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, pin_memory=True, num_workers=cpu_cores)
+train_dataloader_iter = iter(train_dataloader)
+val_dataloader_iter = iter(val_dataloader)
+
 def get_batch(split):
-    # We recreate np.memmap every batch to avoid a memory leak, as per
-    # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
     if split == 'train':
-        data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+        iterator = train_dataloader_iter
+    elif split == 'val':
+        iterator = val_dataloader_iter
     else:
-        data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
-    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
-    if device_type == 'cuda':
-        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
-    else:
-        x, y = x.to(device), y.to(device)
+        assert False
+    x, y = next(iterator)
+    x = x.to(device, nonblocking=True)
+    y = y.to(device, nonblocking=True)
     return x, y
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
